9c1f3382195644c3a1a1484688b95ae47d5269e6
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-05 23:45:48 -0600

    update prod url

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,1 @@
-export const WEBSOCKET_HOST = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/cable' : 'wss://pacman-server.herokuapp.com/';
+export const WEBSOCKET_HOST = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/cable' : 'wss://pacman-server.herokuapp.com/cable';
