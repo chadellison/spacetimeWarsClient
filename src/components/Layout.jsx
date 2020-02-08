88d0efdfe88572c55b2e60faa7fb875a1126766b
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-08 15:43:08 -0600

    allow multiplayer game events

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { WEBSOCKET_HOST } from '../api';
+import { WEBSOCKET_HOST, API_HOST } from '../api';
 import Cable from 'actioncable';
 import Canvas from './Canvas';
 import '../styles/styles.css';
@@ -13,7 +13,7 @@ import {newBoard} from '../helpers/canvasHelper.js'
 import {
   handleDirection,
   handleMouthOpenAngle,
-  handleWrap,
+  handleWall,
   findCollisionCoordinates,
   createPlayer
 } from '../helpers/gameLogic.js';
@@ -34,6 +34,7 @@ class Layout extends React.Component {
   };
 
   componentDidMount() {
+    this.fetchPlayers()
     this.createGameSocket();
     window.addEventListener('keydown', this.handleKeyDown);
     this.interval = setInterval(() => this.movePlayers(), ANAIMATION_FRAME_RATE);
@@ -43,33 +44,56 @@ class Layout extends React.Component {
     clearInterval(this.interval);
   }
 
+  fetchPlayers() {
+    fetch(`${API_HOST}/api/v1/players`)
+      .then((response) => response.json())
+      .then((jsonPlayers) => {
+        const allPlayers = jsonPlayers.map((player) => createPlayer(player));
+        this.setState({players: allPlayers})
+    }).catch((error) => {
+      console.log('ERROR', error)
+    });
+  }
+
   createGameSocket() {
     let cable = Cable.createConsumer(WEBSOCKET_HOST)
     let gameSocket = cable.subscriptions.create({ channel: 'GameDataChannel' },
     {
       connected: () => {},
-      received: (gameData) => this.handleGameData(gameData),
-      create: function(gameEventData) {
+      received: (receivedData) => this.handleGameData(receivedData),
+      create: function(gameData) {
         this.perform('create', {
-          gameEventData: gameEventData
+          gameData: gameData
         });
       },
     });
+
     this.setState({gameSocket: gameSocket})
   };
 
   handleKeyDown = (event) => {
     const keyCode = event.keyCode
     if (this.state.currentPlayerId) {
-      const currentPlayer = this.state.players.filter((player) => player.id === this.state.currentPlayerId)[0];
+      let playerLocations = {};
+      const currentPlayer = this.state.players.filter((player) => {
+        playerLocations[player.id] = player.location
+        return player.id === this.state.currentPlayerId
+      })[0];
 
       if (['left', 'up', 'right', 'down'].includes(KEY_MAP[keyCode]) && KEY_MAP[keyCode] !== currentPlayer.direction) {
-        this.sendGameEvent({playerId: this.state.currentPlayerId, gameEvent: KEY_MAP[keyCode]});
+        this.sendGameEvent({
+          id: this.state.currentPlayerId,
+          gameEvent: KEY_MAP[keyCode],
+          playerLocations: playerLocations
+        });
       };
     } else {
       if (KEY_MAP[keyCode] === 'start') {
         const newPlayerId = this.state.players.length + 1
-        this.sendGameEvent({playerId: newPlayerId, gameEvent: 'start'});
+        this.sendGameEvent({
+          id: newPlayerId,
+          gameEvent: 'start',
+        });
         // use websocket id
         this.setState({currentPlayerId: newPlayerId});
       }
@@ -78,13 +102,15 @@ class Layout extends React.Component {
 
   movePlayers = () => {
     let players = [...this.state.players];
-    players.forEach((player) => {
-      handleMouthOpenAngle(player)
-      handleDirection(player)
-      handleWrap(player, this.state.boardWidth, this.state.boardHeight);
-      this.handleCollision(player, this.state.board);
-    });
-    this.setState({players: players});
+    if (players.length > 0) {
+      players.forEach((player) => {
+        handleMouthOpenAngle(player)
+        handleDirection(player)
+        handleWall(player, this.state.boardWidth, this.state.boardHeight);
+        this.handleCollision(player, this.state.board);
+      });
+      this.setState({players: players});
+    }
   };
 
   handleCollision = (player, board) => {
@@ -97,32 +123,12 @@ class Layout extends React.Component {
   }
 
   handleGameData = response => {
-    const {playerId, gameEvent} = response.gameData;
-    switch (gameEvent) {
-      case 'start':
-        const newPlayer = createPlayer(playerId);
-        this.setState({players: [...this.state.players, newPlayer]});
-        break;
-      case 'left':
-      case 'up':
-      case 'right':
-      case 'down':
-        const updatedPlayers = [...this.state.players].map((player) => {
-          if (player.id === playerId) {
-            player.direction = gameEvent;
-          };
-          return player;
-        });
-
-        this.setState({players: updatedPlayers});
-        break;
-      default:
-        console.log('NO MATCHING EVENTS');
-    }
+    const updatedPlayers = response.gameData.map((player) => createPlayer(player));
+    this.setState({players: updatedPlayers});
   };
 
-  sendGameEvent = (gameEvent) => {
-    this.state.gameSocket.create(gameEvent)
+  sendGameEvent = (gameData) => {
+    this.state.gameSocket.create(gameData)
   };
 
   render = () => {
