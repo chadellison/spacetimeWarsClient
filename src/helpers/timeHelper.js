648924c55c87d282fc907ce6c90653fb259a5703
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-16 09:43:46 -0600

    log round trip time

--- src/helpers/timeHelper.js
+++ src/helpers/timeHelper.js
@@ -1,6 +1,7 @@
 export const findClockDifference = (sentTime, startTime, serverDifference) => {
   const responseTime = Date.now();
   const roundTripTime = responseTime - sentTime;
+  console.log('round trip time: ***********', roundTripTime)
   const clientDifference = responseTime - startTime - roundTripTime;
   return serverDifference - clientDifference;
 }
