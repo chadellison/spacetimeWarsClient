a7eb0831b9b445b196d3b809fb87df890f910688
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-16 19:13:39 -0600

    update server endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
-export const API_HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://tank-game-server.herokuapp.com';
-export const WEBSOCKET_HOST = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/cable' : 'wss://tank-game-server.herokuapp.com/cable';
+export const API_HOST = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://spacetime-wars-server.herokuapp.com';
+export const WEBSOCKET_HOST = process.env.NODE_ENV === 'development' ? 'ws://localhost:3001/cable' : 'wss://spacetime-wars-server.herokuapp.com/cable';
