1378be6b228349fc8edeb8cdedf9eab8d26d2900
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-17 23:05:11 -0600

    animate thruster

--- src/helpers/canvasHelper.js
+++ src/helpers/canvasHelper.js
@@ -9,5 +9,26 @@ export const drawShip = (ctx, player, fighterShip) => {
   ctx.translate(-cx, -cy);
 
   ctx.drawImage(fighterShip, x, y)
+
+  if (player.isAccelerating) {
+    ctx.beginPath();
+    ctx.moveTo(player.location.x - 8, player.location.y + 4 + fighterShip.height / 2);
+    ctx.lineTo(player.location.x - 8, player.location.y - 3 + fighterShip.height / 2);
+    ctx.lineTo(player.location.x, player.location.y - 5 + fighterShip.height / 2);
+    ctx.lineTo(player.location.x, player.location.y + 6 + fighterShip.height / 2);
+
+    let grd = ctx.createLinearGradient(
+      player.location.x - 8,
+      player.location.y - 5 + fighterShip.height / 2,
+      player.location.x,
+      player.location.y + 6 + fighterShip.height / 2
+    );
+    grd.addColorStop(0, "#5c93e6");
+    grd.addColorStop(1, "#f0f6ff");
+
+    ctx.stroke();
+    ctx.fillStyle = grd;
+    ctx.fill();
+  }
   ctx.restore()
 }
