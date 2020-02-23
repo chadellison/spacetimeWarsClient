fd4522807518a0b231102403b3c03a74de07fc3a
Chad Ellison <chad.ellison0123@gmail.com>
2020-02-22 21:31:55 -0600

    add error handled sound

--- src/helpers/audioHelpers.js
+++ src/helpers/audioHelpers.js
@@ -7,15 +7,26 @@ thruster.loop = true;
 const cannon = new Audio(cannonAudio);
 
 export const handleAudio = (player) => {
-  // if (player.accelerate) {
-  //   thruster.currentTime = 0
-  //   thruster.play();
-  // } else {
-  //   thruster.pause();
-  // }
-  //
-  // if (player.fire) {
-  //   cannon.currentTime = 0
-  //   cannon.play();
-  // }
+  if (player.accelerate) {
+    thruster.currentTime = 0
+    const playPromise = thruster.play();
+    if (playPromise !== undefined) {
+      playPromise.catch(() => {thruster.play()})
+    }
+  } else {
+    const pausePromise = thruster.pause();
+    if (pausePromise !== undefined) {
+      pausePromise.catch(() => {thruster.pause()})
+    }
+  }
+
+  if (player.fire) {
+    cannon.currentTime = 0
+    const cannonPromise = cannon.play();
+    if (cannonPromise !== undefined) {
+      cannonPromise.catch(() => {
+        cannonPromise.play()
+      });
+    }
+  }
 }
