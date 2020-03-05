f1c3b1fb4450e6ab40686eb37f3d416f15b632e7
Chad Ellison <chad.ellison0123@gmail.com>
2020-03-05 17:25:50 -0600

    refactor

--- src/components/Hitpoints.jsx
+++ src/components/Hitpoints.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {PieChart, Pie, Label} from 'recharts'
 import '../styles/hitpoints.css';
+import {findColor} from '../helpers/colorHelpers.js';
 
 const calculateAngle = (hitpoints, maxHitpoints) => {
   const value = Math.round(hitpoints * 360 / maxHitpoints)
@@ -8,6 +9,7 @@ const calculateAngle = (hitpoints, maxHitpoints) => {
 };
 
 export const Hitpoints = ({hitpoints, maxHitpoints}) => {
+  const color = findColor(hitpoints, maxHitpoints);
   return (
     <div className="hitpoints">
       <PieChart width={100} height={100}>
@@ -22,10 +24,10 @@ export const Hitpoints = ({hitpoints, maxHitpoints}) => {
           startAngle={0}
           endAngle={calculateAngle(hitpoints, maxHitpoints)}
           outerRadius={40}
-          fill="red"
+          fill={color}
           text={'hp'}
         >
-          <Label style={{fill: 'red'}} value={hitpoints > 0 ? hitpoints : 0} offset={0} position="center"/>
+          <Label style={{fill: color}} value={hitpoints > 0 ? hitpoints : 0} offset={0} position="center"/>
         </Pie>
       </PieChart>
     </div>
---
+++ src/helpers/colorHelpers.js
@@ -0,0 +1,31 @@
+const hslToRgb = (h, s, l) => {
+  let r, g, b;
+
+  if (s === 0) {
+      r = g = b = l;
+  } else {
+    let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    let p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+const hue2rgb = (p, q, t) => {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1/6) return p + (q - p) * 6 * t;
+  if (t < 1/2) return q;
+  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  return p;
+};
+
+export const findColor = (hitpoints, maxHitpoints) => {
+  const percentage = Math.round(hitpoints / maxHitpoints * 100);
+  const hue = percentage * 1.2 / 360;
+  const rgb = hslToRgb(hue, 1, .5);
+  return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+};
