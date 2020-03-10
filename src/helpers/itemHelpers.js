641cae4ec038b9b16de7d57d7d0ac63ea29ae76d
Chad Ellison <chad.ellison0123@gmail.com>
2020-03-10 02:08:03 -0400

    fix items bug

--- src/helpers/itemHelpers.js
+++ src/helpers/itemHelpers.js
@@ -1,23 +1,25 @@
 export const handleItems = (player) => {
-  const currentTime = Date.now();
-  player.items.forEach((item) => {
-    switch (item.id) {
-      case 1:
-      if (currentTime - item.lastUpdated >= 120 * 1000) {
-        if (player.hitpoints < player.maxHitpoints / 4) {
-          item.lastUpdated = currentTime;
-          player.hitpoints += (player.maxHitpoints / 2);
+  if (player.items) {
+    const currentTime = Date.now();
+    player.items.forEach((item) => {
+      switch (item.id) {
+        case 1:
+        if (currentTime - item.lastUpdated >= 120 * 1000) {
+          if (player.hitpoints < player.maxHitpoints / 4) {
+            item.lastUpdated = currentTime;
+            player.hitpoints += (player.maxHitpoints / 2);
+          }
         }
+        break;
+        case 2:
+        if (currentTime - item.lastUpdated >= 1000) {
+          item.lastUpdated = currentTime;
+          const newHitpoints = Math.round(player.maxHitpoints * 0.01 + player.hitpoints);
+          player.hitpoints = newHitpoints > player.maxHitpoints ? player.maxHitpoints : newHitpoints;
+        }
+        break;
+        default:
       }
-      break;
-      case 2:
-      if (currentTime - item.lastUpdated >= 1000) {
-        item.lastUpdated = currentTime;
-        const newHitpoints = Math.round(player.maxHitpoints * 0.01 + player.hitpoints);
-        player.hitpoints = newHitpoints > player.maxHitpoints ? player.maxHitpoints : newHitpoints;
-      }
-      break;
-      default:
-    }
-  })
+    })
+  };
 };
