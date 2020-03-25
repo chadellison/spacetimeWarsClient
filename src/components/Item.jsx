e65d914a7eb912867500cef9fd1b6cc20f886af1
Chad Ellison <chad.ellison0123@gmail.com>
2020-03-25 12:53:45 -0500

    do not allow player to get the same item multiple times

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/ship.css';
 import {getUpdatedPlayers} from '../helpers/gameLogic.js';
+import {getItem} from '../helpers/itemHelpers.js';
 import {notEnoughResources, gong} from '../constants/settings.js';
 import {ITEMS} from '../constants/items.js';
 
 const handleClick = (currentPlayer, item, players, updateState) => {
   const gold = currentPlayer.gold - ITEMS[item.index].price;
-  if (gold >= 0) {
+  if (gold >= 0 && !getItem(currentPlayer.items, item.id)) {
     const player = {
       ...currentPlayer,
       items: {
