8157988c6a2607f14ceac26383f466bbfb93f51a
Chad Ellison <chad.ellison0123@gmail.com>
2020-03-26 17:55:35 -0500

    more gracefully handle sound

--- src/helpers/audioHelpers.js
+++ src/helpers/audioHelpers.js
@@ -4,13 +4,13 @@ import {getItem} from '../helpers/itemHelpers.js';
 
 export const handleAudio = (player) => {
   const hasStealth = getItem(player.items, 3);
-  if (player.accelerate && !hasStealth) {
+  if (player.gameEvent === 'up' && !hasStealth) {
     thruster.currentTime = 0
     const playPromise = thruster.play();
     if (playPromise !== undefined) {
       playPromise.catch((e) => console.log(e));
     }
-  } else {
+  } else if (player.gameEvent === 'upStop') {
     const pausePromise = thruster.pause();
     if (pausePromise !== undefined) {
       pausePromise.catch((e) => console.log(e));
