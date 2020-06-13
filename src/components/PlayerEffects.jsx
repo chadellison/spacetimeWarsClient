63416640633b2a239b20c6b24061728b1293f1fa
Chad Ellison <chad.ellison0123@gmail.com>
2020-06-13 13:19:32 -0500

    make effects more visible

--- src/components/PlayerEffects.jsx
+++ src/components/PlayerEffects.jsx
@@ -5,7 +5,7 @@ export const PlayerEffects = ({effects}) => {
   return Object.values(effects).map((effect) => {
     return (
       <div className="playerEffectData" key={'playerEffect' + effect.id}>
-        <div style={{color: effect.color}}>{effect.name}</div>
+        <div style={{color: 'white'}}>{effect.name}</div>
       </div>
     )
   });
