1defe86549ab14ea3e1ab620f176284302f9b5e8
Chad Ellison <chad.ellison0123@gmail.com>
2020-06-24 18:27:36 -0500

    make rate dynamic

--- src/helpers/animation.js
+++ src/helpers/animation.js
@@ -1,5 +1,5 @@
 export class Animation {
-  constructor(spriteImage, width, height, renderSize, rowCount, columnCount) {
+  constructor(spriteImage, width, height, renderSize, rowCount, columnCount, rate) {
     this.coordinates = {x: 0, y: 0}
     this.spriteImage = spriteImage
     this.width = width
@@ -7,16 +7,23 @@ export class Animation {
     this.renderSize = renderSize
     this.rowCount = rowCount
     this.columnCount = columnCount
+    this.rate = rate
+    this.startRate = rate
   }
 
   updateFrame = () => {
-    if (this.coordinates.x >= this.width * (this.rowCount - 1) && this.coordinates.y >= this.height * (this.columnCount - 1)) {
-      this.coordinates = {x: 0, y: 0}
-    } else if (this.coordinates.x >= this.width * (this.rowCount - 1)) {
-      this.coordinates.x = 0
-      this.coordinates.y += this.height
+    if (this.rate === 0) {
+      if (this.coordinates.x >= this.width * (this.rowCount - 1) && this.coordinates.y >= this.height * (this.columnCount - 1)) {
+        this.coordinates = {x: 0, y: 0}
+      } else if (this.coordinates.x >= this.width * (this.rowCount - 1)) {
+        this.coordinates.x = 0
+        this.coordinates.y += this.height
+      } else {
+        this.coordinates.x += this.width
+      }
+      this.rate = this.startRate
     } else {
-      this.coordinates.x += this.width
+      this.rate -= 1
     }
   }
 }
