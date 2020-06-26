8505e6b90c6b50ab51abcb0e9928529553151dde
Chad Ellison <chad.ellison0123@gmail.com>
2020-06-26 16:02:23 -0500

    upgrade information modal

--- src/components/InformationModal.jsx
+++ src/components/InformationModal.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import '../styles/modal.css';
+import redBomber from '../images/redBomber.png';
+import blueBomber from '../images/blueBomber.png';
 
 export const InformationModal = ({updateState}) => {
   return (
     <div className="modal">
-      <div className="informationTitle">How to play</div>
-      <div className="informationText">
+      <div className="informationTitle">Space Wars</div>
+      <div className="informationContext">
         There are two teams (Red and Blue).
       </div>
-      <div className="informationText">
-        The object of the game is to prevent your opponents' "bombers"
+      <div className="informationContext">
+        The object of the game is to prevent your opponents'
       </div>
-      <div className="informationText">
-        from crossing your edge of the screen.
+      <div className="informationTitle">"Bombers"</div>
+        <img src={redBomber} className="infoImage" alt="bomber" />
+        <img src={blueBomber} className="infoImage blueShipInfo" alt="bomber" />
+      <div className="informationContext">
+        from weakening your defenses by crossing your team's edge of the screen.
       </div>
-        <div className="informationText">Press any key to start or click the Start button</div>
-        <div className="informationText">Select a Ship from the menu</div>
-        <div className="informationText">Select a weapon</div>
-        <div className="informationText">Click "start" to begin playing.</div>
+      <div className="informationContext">
+        Each bomber that crosses the threshold reduces your team's defenses (starts at 100)
+      </div>
+      <div className="informationContext">
+        The defense values will be located on the left during game play.
+      </div>
+      <div className="informationContext">
+        The first team to bring the opponents defenses down to 0 wins!
+      </div>
+      <div className="informationTitle">How to play</div>
+      <div className="informationText">Press any key to start or click the Start button</div>
+      <div className="informationText">Select a Ship from the menu</div>
+      <div className="informationText">Select a weapon</div>
+      <div className="informationText">Click "start" to begin playing.</div>
       <div className="informationTitle">Controls</div>
       <div className="informationText">
         <span className="instructionControl">"Up":</span> accelerate
